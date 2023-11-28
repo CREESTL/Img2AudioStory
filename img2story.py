a3b16cde85c1e9deee9fd0b3bb57c073af5ada64
CREESTL <kroymw3@yandex.ru>
2023-11-28 13:28:28 +0300

    Fix rq

--- img2story.py
+++ img2story.py
@@ -1,8 +1,8 @@
-'''
+"""
 
 Turn image to text. Write a story based on that text. Read the story
 
-'''
+"""
 
 
 from dotenv import find_dotenv, load_dotenv
@@ -19,24 +19,28 @@ HF_TOKEN = os.getenv("HF_API_TOKEN")
 
 # Image to text
 def img2text(url):
-    image_to_text = pipeline("image-to-text", model="Salesforce/blip-image-captioning-large", device=1)
-    text = image_to_text(url)[0]['generated_text']
+    image_to_text = pipeline(
+        "image-to-text", model="Salesforce/blip-image-captioning-large", device=1
+    )
+    text = image_to_text(url)[0]["generated_text"]
     return text
+
 
 # Text to story
 def text2story(text):
     text_to_story = pipeline("text-generation", model="gpt2", device=1)
-    story = text_to_story(text, max_length=200, do_sample=True)[0]['generated_text']
+    story = text_to_story(text, max_length=200, do_sample=True)[0]["generated_text"]
     return story
+
 
 # Story to speech
 # Use API this time
 def story2speech(story):
-    API_URL = "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
+    API_URL = (
+        "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
+    )
     headers = {"Authorization": f"Bearer {HF_TOKEN}"}
-    payloads = {
-        "inputs": story
-    }
+    payloads = {"inputs": story}
     response = requests.post(API_URL, headers=headers, json=payloads)
 
     with open("audio.flac", "wb") as file:
